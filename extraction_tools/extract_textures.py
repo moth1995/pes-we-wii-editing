2177ea9a84dca29c6668095bf9a0dfd9a369a7ee
moth1995 <marcos-flores@live.com.ar>
2026-01-19 09:06:10 -0300

    update palette gathering with newer methods on reversebox

--- extraction_tools/extract_textures.py
+++ extraction_tools/extract_textures.py
@@ -1,89 +1,149 @@
 import os
-from reversebox.io_files.file_handler import FileHandler
+import struct
+
+import reversebox.io_files.bytes_helper_functions as BytesHelper
 from reversebox.compression.compression_zlib import ZLIBHandler
+from reversebox.image import byte_swap
 from reversebox.image.image_decoder import ImageDecoder
 from reversebox.image.image_formats import ImageFormats
-from reversebox.image import byte_swap
-from reversebox.image.swizzling import swizzle_gamecube
 from reversebox.image.pillow_wrapper import PillowWrapper
-import reversebox.io_files.bytes_helper_functions as BytesHelper
-import struct
+from reversebox.image.swizzling import swizzle_gamecube
+from reversebox.io_files.file_handler import FileHandler
 
 TEXTURE_MAGIC_NUMBER = 0x29857294
 zlib_handler = ZLIBHandler()
 ENDIANESS = ">"
 ENDIANESS_STR = "big"
 
-def extract_texture(data:bytearray, file_name:str, pos: int = 0):
+
+def extract_texture(data: bytearray, file_name: str, pos: int = 0):
     cursor = pos
-    magic_number = BytesHelper.get_uint32(data[cursor: cursor + 4], ENDIANESS)
+    magic_number = BytesHelper.get_uint32(data[cursor : cursor + 4], ENDIANESS)
 
     if magic_number != TEXTURE_MAGIC_NUMBER:
         raise Exception("Invalid file format")
 
-    cursor+=8
-    size = BytesHelper.get_uint32(data[cursor: cursor + 4], ENDIANESS)
-    cursor+=8
-    image_buffer_offset = BytesHelper.get_uint16(data[cursor: cursor + 2], ENDIANESS)
-    cursor+=2
-    image_palette_offset = BytesHelper.get_uint16(data[cursor: cursor + 2], ENDIANESS)
-    cursor+=2
-    width = BytesHelper.get_uint16(data[cursor: cursor + 2], ENDIANESS)
-    cursor+=2
-    height = BytesHelper.get_uint16(data[cursor: cursor + 2], ENDIANESS)
+    cursor += 8
+    size = BytesHelper.get_uint32(data[cursor : cursor + 4], ENDIANESS)
+    cursor += 8
+    image_buffer_offset = BytesHelper.get_uint16(data[cursor : cursor + 2], ENDIANESS)
+    cursor += 2
+    image_palette_offset = BytesHelper.get_uint16(data[cursor : cursor + 2], ENDIANESS)
+    cursor += 2
+    width = BytesHelper.get_uint16(data[cursor : cursor + 2], ENDIANESS)
+    cursor += 2
+    height = BytesHelper.get_uint16(data[cursor : cursor + 2], ENDIANESS)
     cursor = pos
 
     decoder = ImageDecoder()
     wrapper = PillowWrapper()
 
     pil_image = None
+    converted_data: bytes | None = None
 
     if image_buffer_offset == image_palette_offset and image_buffer_offset < size:
         # then its probably a dtx1 texture with GC byteswap
 
-        image_data:bytes = data[cursor + image_buffer_offset: cursor + size]
+        image_data: bytes = data[cursor + image_buffer_offset : cursor + size]
 
         image_data = byte_swap.swap_byte_order_gamecube(image_data, width, height)
 
-        converted_data: bytes = decoder.decode_compressed_image(image_data, width, height, ImageFormats.BC1_DXT1)
-        pil_image = wrapper.get_pillow_image_from_rgba8888_data(converted_data, width, height)
+        converted_data = decoder.decode_compressed_image(
+            image_data, width, height, ImageFormats.BC1_DXT1
+        )
 
     elif image_buffer_offset and image_buffer_offset < size:
-        image_data:bytes = data[cursor + image_buffer_offset: cursor + size]
+        image_data: bytes = data[cursor + image_buffer_offset : cursor + size]
+        palette_data: bytes = data[
+            cursor + image_palette_offset : cursor + image_buffer_offset
+        ]
 
-        image_data = swizzle_gamecube.unswizzle_gamecube(image_data, width, height, 8)
-        converted_data: bytes = decoder.decode_image(image_data, width, height, ImageFormats.GRAY8)
-        pil_image = wrapper.get_pillow_image_from_rgba8888_data(converted_data, width, height)
+        if len(palette_data) == 1024:
+            image_data = swizzle_gamecube.unswizzle_gamecube(
+                image_data, width, height, 8
+            )
+            converted_data = decoder.decode_indexed_image(
+                image_data,
+                palette_data,
+                width,
+                height,
+                ImageFormats.PAL8,
+                ImageFormats.IA_X2_GRAB,
+                "big",
+                "big",
+            )
+        elif len(palette_data) == 64:
+            image_data = swizzle_gamecube.unswizzle_gamecube(
+                image_data, width, height, 8
+            )
+            converted_data = decoder.decode_indexed_image(
+                image_data,
+                palette_data,
+                width,
+                height,
+                ImageFormats.PAL4,
+                ImageFormats.IA_X2_GRAB,
+                "big",
+                "big",
+            )
+        elif len(palette_data) == 512:
+            image_data = swizzle_gamecube.unswizzle_gamecube(
+                image_data, width, height, 8
+            )
+            converted_data = decoder.decode_indexed_image(
+                image_data,
+                palette_data,
+                width,
+                height,
+                ImageFormats.PAL8,
+                ImageFormats.BGR565,
+                "big",
+                "big",
+            )
+        else:
+            print(f"unk palette size: {len(palette_data)}")
+
+    if converted_data is None:
+        return
+
+    pil_image = wrapper.get_pillow_image_from_rgba8888_data(
+        converted_data, width, height
+    )
 
     if pil_image is None:
         return
 
-    out_path = file_name + ".png"
+    parent_dir = os.path.dirname(file_name)
+    file_name_with_ext = os.path.basename(file_name)
+
+    out_path = os.path.join(parent_dir, "0_text_textures", file_name_with_ext) + ".png"
 
     print(out_path)
 
-    pil_image.save(out_path)
+    pil_image.save(out_path)  # type: ignore
 
 
-def find_textures(data:bytearray) -> list:
+def find_textures(data: bytearray) -> list:
     positions = []
 
     pos = 0
-    pos = data.find(struct.pack(f"{ENDIANESS}I",TEXTURE_MAGIC_NUMBER), pos)
+    pos = data.find(struct.pack(f"{ENDIANESS}I", TEXTURE_MAGIC_NUMBER), pos)
 
     while pos != -1:
         positions.append(pos)
-        pos = data.find(struct.pack(f"{ENDIANESS}I",TEXTURE_MAGIC_NUMBER), pos + 4)
+        pos = data.find(struct.pack(f"{ENDIANESS}I", TEXTURE_MAGIC_NUMBER), pos + 4)
 
     return positions
 
-def pes_decompress(data:bytearray, pos:int = 0):
+
+def pes_decompress(data: bytearray, pos: int = 0):
     """
     This function could be totally improved, but so far this was what worked just fine to extract
     """
-    return zlib_handler.decompress_data(data[pos + 32:])
+    return zlib_handler.decompress_data(data[pos + 32 :])
 
-def extract_textures_from_folder(folder_path:str, filter_prefixes:tuple):
+
+def extract_textures_from_folder(folder_path: str, filter_prefixes: tuple):
     for file in os.listdir(folder_path):
         if not file.endswith(filter_prefixes):
             continue
@@ -94,11 +154,16 @@ def extract_textures_from_folder(folder_path:str, filter_prefixes:tuple):
         value = None
         file_reader = None
         try:
-            file_reader = FileHandler(file_path=file_path, open_mode="rb", endianess_str=ENDIANESS_STR)
+            file_reader = FileHandler(
+                file_path=file_path, open_mode="rb", endianess_str=ENDIANESS_STR
+            )
+            if not file_reader.get_file_size():
+                print(f"{file_path} is an empty file")
+                continue
             value = file_reader.read_uint32()
         except Exception as e:
             print(e)
-        
+
         if file_reader is None or file_reader.file is None:
             continue
 
@@ -113,7 +178,7 @@ def extract_textures_from_folder(folder_path:str, filter_prefixes:tuple):
 
         if value == TEXTURE_MAGIC_NUMBER:
             extract_texture(file_data, f"{file_path}_{0:03d}", 0)
-        elif (value >> 8 & 0xff) == 0x01:
+        elif (value >> 8 & 0xFF) == 0x01:
             try:
                 decompress_data = bytearray(pes_decompress(file_data))
                 file_path += f"_{0:03d}"
@@ -122,6 +187,8 @@ def extract_textures_from_folder(folder_path:str, filter_prefixes:tuple):
                     extract_texture(decompress_data, f"{file_path}_{i:03d}", position)
             except Exception as e:
                 print(e)
+
+
 def main():
 
     # # example to extract texture from a folder
@@ -149,10 +216,8 @@ def main():
     # for i, position in enumerate(positions):
     #     extract_texture(decompress_data, f"{file_path}_{i:03d}", position)
 
-
     return
 
 
 if __name__ == "__main__":
     main()
-
